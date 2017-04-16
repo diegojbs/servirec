901328e545bbd1fcefc3c64eb539c495865d6287
Diego Suarez <diego-jbs@hotmail.com>
2017-04-15 20:50:45 -0500

    Se agrega chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -207,6 +207,19 @@
 <!-- MDB core JavaScript -->
 <script type="text/javascript" src="{{ asset('js/mdb.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/56faa6ca5de6a2ac05c9a2fa/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
 
